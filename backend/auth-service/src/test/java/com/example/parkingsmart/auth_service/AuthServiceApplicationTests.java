6c9af33b369118daf639117a62916761bcf315d2
Nicolasperlo29 <nicolasperlo29@gmail.com>
2025-12-27 18:54:05 -0300

    ci: include auth-service in workflow

---
+++ backend/auth-service/src/test/java/com/example/parkingsmart/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingsmart.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
