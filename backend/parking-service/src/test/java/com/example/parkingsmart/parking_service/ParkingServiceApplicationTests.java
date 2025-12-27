3aa081709e03444fb8056e19aca9e2169a37ec25
Nicolasperlo29 <nicolasperlo29@gmail.com>
2025-12-27 15:59:03 -0300

    Initial microservices structure: gateway, parking, payment + frontend

---
+++ backend/parking-service/src/test/java/com/example/parkingsmart/parking_service/ParkingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parkingsmart.parking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
